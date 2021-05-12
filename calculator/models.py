d9b5d58bb04b8c942d6049bf295c32b7f817fd60
professor <yongyaatom@gmail.com>
2021-05-12 09:11:14 +0545

    use image in news page

--- calculator/models.py
+++ calculator/models.py
@@ -8,6 +8,7 @@ class News(models.Model):
     date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
     reporter = models.CharField(max_length=40)
     news_header = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='pics')
     news = models.TextField()
 
 
